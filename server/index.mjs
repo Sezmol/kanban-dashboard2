f7b0ee65baae6e437f6b066cfbf84540e78bb221
sezmol <etho4556@gmail.com>
2024-03-16 17:31:54 +0300

    add server

---
+++ server/index.mjs
@@ -0,0 +1,27 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.emit("message", "Hello from server");
+});
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
